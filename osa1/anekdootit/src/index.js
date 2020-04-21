3f7ff0b8a6f58fd862237858a39ae5ff49384825
Fransilia <mathilda.liljestrand@gmail.com>
2020-04-21 14:47:22 +0300

    anekdootti tehty

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -1,47 +1,67 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-
-
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
 )
 
-const App = (props) => {
-
-const [selected, setSelected] =useState(Math.floor((Math.random() * 7) + 0))
-
-const nextA = () => {
-  setSelected(Math.floor((Math.random() * 7) + 0))
-}
-
-
+const Votes = (props) => {
   return (
     <div>
-      {props.anecdotes[selected]}
       <p>
-         has votes
+        has {props.votes[props.selected]} votes
       </p>
-      
-      <p>
-        
-      <Button
-        handleClick={nextA}
-        text='next'       
-      />
-      <Button
-        text='vote'
-        />
-      </p>
-
-    
     </div>
+
   )
 }
 
+const App = (props) => {
 
+  const [selected, setSelected] =useState(Math.floor((Math.random() * 7) + 0))
+  const [votes, setVotes] =useState(props.pisteLista)
+
+  const nextA = () => {
+    setSelected(Math.floor((Math.random() * 7) + 0))
+  }
+
+  const addPoint = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
+  const isoin = (Math.max(...votes))
+  const paikka = (votes.indexOf(isoin))
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {props.anecdotes[selected]}
+    <div>
+      <Votes votes={votes} selected = {selected}/>
+    </div>     
+      <div>    
+        <Button
+          handleClick={nextA}
+          text='next'       
+        />
+        <Button
+          text='vote'
+          handleClick={addPoint}
+          />
+      </div> 
+
+      <h1>Anecdote with most votes</h1>
+      {props.anecdotes[paikka]}
+      <p>
+        has {isoin} votes
+      </p>
+    </div>
+  )
+}
 
 const anecdotes = [
   'If it hurts, do it more often',
@@ -53,7 +73,11 @@ const anecdotes = [
   'I do not like computer science jokes... not one bit.'
 ]
 
+const pisteLista = Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0);
+console.log(pisteLista)
+
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
+  <App anecdotes={anecdotes} pisteLista={pisteLista}/>,
+  
   document.getElementById('root')
 )
